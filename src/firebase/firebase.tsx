8134ab5a33210da136ea6e024a04ad1cd04da1ae
AmirelIbrahimi <amir.okido@gmail.com>
2025-06-02 08:42:44 +0200

    firebase connectie(misschien) gefixt.

--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -1,7 +1,11 @@
-// firebase.ts
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyDIab5pd5vj0hSCa1AisTB1Cy1t2t-Ngbk",
     authDomain: "l2p4frontend.firebaseapp.com",
@@ -12,5 +16,6 @@ const firebaseConfig = {
     measurementId: "G-MH08GCCV0P"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
